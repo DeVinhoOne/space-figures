a79e3ba3085dbf5fc41c83f67a88136c01db6d9a
dawid <caushered@gmail.com>
2021-04-25 14:40:37 +0200

    create cube figure and basic ui

---
+++ src/com/devinho/spacefigures/Cube.java
@@ -0,0 +1,31 @@
+package com.devinho.spacefigures;
+
+public class Cube {
+
+    private double sideLength;
+
+    public Cube(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double area() {
+        return 8 * (sideLength * sideLength);
+    }
+
+    public double volume() {
+        return sideLength * sideLength * sideLength;
+    }
+
+    @Override
+    public String toString() {
+        return "The cube has side length of " + this.sideLength + ", area " + this.area() + ", volume " + this.volume();
+    }
+}
